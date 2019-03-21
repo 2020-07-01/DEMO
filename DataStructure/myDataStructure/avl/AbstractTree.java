bcc7d4b869ee98fa61f24aa2bf89709906193d68
yuqiang <>
2019-03-21 22:55:11 +0800

    AVL的实现
    AVL的实现

---
+++ DataStructure/myDataStructure/avl/AbstractTree.java
@@ -0,0 +1,29 @@
+package avl;
+
+/**
+ * 抽象类部分实现tree
+ * @author qiang
+ *
+ * @param <E>
+ */
+
+public abstract class AbstractTree<E> implements Tree<E>{
+	 
+	@Override
+	public void inorder() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void preorder() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postorder() {
+		// TODO Auto-generated method stub
+		
+	}
+}
