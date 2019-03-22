c931b76ece4e91b95ba68ad96d4a5b5625582504
yuqiang <>
2019-03-22 23:44:47 +0800

    代码规范
    代码规范

--- DataStructure/src/collectionTest/Test03.java
+++
@@ -1,9 +0,0 @@
-package collectionTest;
-
-public class Test03 {
-	static String string ;
-	public static void main(String[] args) {
-		 
-		System.out.println(string);
-	}
-}
--- TestProxy/src/com/HelloWorld.java
+++
@@ -1,5 +0,0 @@
-package com;
-
-public interface HelloWorld {
-	public void sayHelloWorld();
-}
