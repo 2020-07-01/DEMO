59ed7b884a6487078ecf253fe035567b2b40c594
yuqiang <2422321558@qq.com>
2019-03-23 14:50:50 +0800

    二叉树的实现
    二叉树的实现

---
+++ JavaExercise/src/main/java/dataStructure/bst/TreeNode.java
@@ -0,0 +1,13 @@
+package dataStructure.bst;
+
+public class TreeNode<E> {
+	protected E element;
+	protected TreeNode<E> left;
+	protected TreeNode<E> right;
+	
+	public TreeNode(E data) {
+		this.element = data;
+		// TODO Auto-generated constructor stub
+	}
+	
+}
