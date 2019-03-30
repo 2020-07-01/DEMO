f053fa4cc85cdf97a2a6fce053d5b57acd50eaa7
yuqiang <2422321558@qq.com>
2019-03-30 22:27:45 +0800

    字符串中空格的替换
    字符串中空格的替换

--- JavaExercise/src/main/java/nowcoder/String_blank.java
+++ JavaExercise/src/main/java/nowcoder/String_blank.java
@@ -3,22 +3,27 @@ package nowcoder;
 import java.util.Scanner;
 
 /**
- * 替换
+ * 字符传中空隔的替换
  * @author qiang
  *
  */
 public class String_blank {
+	public String replaceSpace(StringBuffer str) {
+		
+		String string = str.toString();
+		String newString = string.replaceAll(" ", "%20");
+		 
+		return newString;
+    }
+	
 	public static void main(String[] args) {
-		
+		String_blank test = new String_blank();
 		Scanner input = new Scanner(System.in);
-		
 		String string = input.nextLine();
 		StringBuffer stringBuffer = new StringBuffer(string);
+		String newString = test.replaceSpace(stringBuffer);
+		System.out.println(newString);
 		
-		String a = stringBuffer.toString().replaceAll(" ", "%20");
 		
-		System.out.println(a);
 	}
-	 
-
 }
