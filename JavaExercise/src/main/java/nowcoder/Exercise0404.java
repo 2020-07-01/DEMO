4fbd8332c6ce9bcfcd0ec78bdc2daadf1fe2ff96
yuqiang <2422321558@qq.com>
2019-04-14 22:12:54 +0800

    日常编码
    日常编码

--- JavaExercise/src/main/java/nowcoder/Exercise0404.java
+++ JavaExercise/src/main/java/nowcoder/Exercise0404.java
@@ -66,12 +66,12 @@ public class Exercise0404 {
 	}
 
 	public static void main(String[] args) {
-		int[] array = { 1, 2, 4, 6, 8, 9, 5, 4, 2, 4, 5, 6, 7745, 5, 2 };
+		/*int[] array = { 1, 2, 4, 6, 8, 9, 5, 4, 2, 4, 5, 6, 7745, 5, 2 };
 		reOrderArray(array);
 		for (int i = 0; i < array.length; i++) {
 			System.out.print(array[i]);
 		}
-
+		*/
 		System.out.println();
 		Scanner input = new Scanner(System.in);
 		String string = input.nextLine();
