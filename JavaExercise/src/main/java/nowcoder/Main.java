76dc1b88dae8d929cf79966251bd117bd400247b
yuqiang <2422321558@qq.com>
2019-04-24 21:35:05 +0800

    日常编码

--- JavaExercise/src/main/java/nowcoder/Main.java
+++ JavaExercise/src/main/java/nowcoder/Main.java
@@ -3,6 +3,8 @@ package nowcoder;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import jdk.internal.org.objectweb.asm.tree.IntInsnNode;
+
 /**
  * @ClassName:  Main   
  * @Description:京东笔试
@@ -11,37 +13,68 @@ import java.util.Scanner;
  */
 public class Main {
 
-	public static void main(String[] args) {
 
-		int m;
-		Scanner input = new Scanner(System.in);
-		m = input.nextInt();
-
-		ArrayList<String> list = new ArrayList<String>();
-
-		int i = 0;
-		while (i <= (m + 1)) {
-			i++;
-			list.add(input.nextLine());
+/*请完成下面这个函数，实现题目要求的功能
+当然，你也可以不按照下面这个模板来作答，完全按照自己的想法来 ^-^ 
+******************************开始写代码******************************/
+    static int string2int(String str) {
+    	//转换为字符
+    	char[] c = str.toCharArray();
+    	ArrayList<Character> list = null;
+    	
+		//遍历查找为0的情况
+		for (int i = 0; i < c.length; i++) {
+			//如果有不是整数的字符，返回0
+			if (c[i] > 57 || c[i] == 47 || c[i] < 46)
+				return 0;
 		}
-
-		//字符串T
-		String T = list.remove(list.size() - 1);
-		int count = 0;
-		for (int j = 1; j < list.size(); j++) {
-			//如果存在此元素，
-			if (T.contains(list.get(j))) {
-				count++;
-				int a = T.indexOf(list.get(j));
-				for (int k = a; k <= list.get(j).length(); k++) {
-					list.set(k, "0");
-				}
+		
+		 
+			for (int i = 0; i < c.length; i++) {
+					if(c[i] != 0 && c[i+1] == 0)
+					{
+						
+					}
 			}
+	 
+		
+		return 0;
+		
+		
+		
+ 
+    	
+		/*else {
+			//保存小数点前面的数字
+			int i = 0;
+			while(c[i] != 47)
+			{
+				cc[i] = c[i];
+				i++;
+			}
+			
+			int p = Integer.parseInt(cc.toString());
+			return p;
+		}*/
+    	 
+    }
+/******************************结束写代码******************************/
 
-		}
 
-		System.out.println(count);
+    public static void main(String[] args){
+    	System.out.println("shuru ");
+        Scanner in = new Scanner(System.in);
+        int res;
+            
+        String _str;
+        try {
+            _str = in.nextLine();
+        } catch (Exception e) {
+            _str = null;
+        }
+  
+        res = string2int(_str);
+        System.out.println(String.valueOf(res));    
 
-	}
-
+    }
 }
