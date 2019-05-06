9f3db2a1621d266cc14d0a96d8e4b700a28d64ca
yuqiang <2422321558@qq.com>
2019-05-06 22:23:25 +0800

    I/O学习

---
+++ src/main/java/io/TestCommonsIO.java
@@ -0,0 +1,20 @@
+package io;
+
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+
+/**
+ * @ClassName:  TestCommonsIO   
+ * @Description:测试commons-io-jar
+ * @author: caiji
+ * @date: 2019年5月6日 下午3:11:47
+ */
+public class TestCommonsIO {
+
+	public static void main(String[] args) {
+
+		//目录大小
+		long len = FileUtils.sizeOf(new File("src/main/java/io"));
+		System.out.println(len);
+	}
+}
