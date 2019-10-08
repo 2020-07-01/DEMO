f370cb926012ef65372a7a8b25bc4ec45a1e716c
yq-java <2422321558@qq.com>
2019-10-08 12:55:27 +0800

    代码规范

---
+++ src/main/java/string/TestFormatter.java
@@ -0,0 +1,21 @@
+package string;
+
+import java.util.Formatter;
+
+/**
+ * @author :qiang
+ * @date :2019/10/7 下午12:55
+ * @description :Formatter类学习
+ * @other :
+ */
+public class TestFormatter {
+
+
+
+    public static void main(String[] args) {
+        Formatter formatter = new Formatter(System.out);
+        formatter.format("123");
+
+
+    }
+}
---
+++ src/main/java/test/Main.java
@@ -0,0 +1,23 @@
+package test;
+
+/**
+ * @author :qiang
+ * @date :2019/10/7 下午4:34
+ * @description :
+ * @other :
+ */
+
+public class Main {
+
+
+    public Main(String string) {
+        System.out.println(string);
+    }
+
+
+   
+
+    public static void main(String[] args) {
+        System.out.println("main");
+    }
+}
