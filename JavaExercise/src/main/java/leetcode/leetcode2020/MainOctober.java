bf96c1b723d22273ff1a09c4e43fb7457ef850c1
2020-07-01 <2422321558@qq.com>
2020-10-01 12:53:51 +0800

    70. 爬楼梯

--- JavaExercise/src/main/java/leetcode/leetcode2020/MainOctober.java
+++ JavaExercise/src/main/java/leetcode/leetcode2020/MainOctober.java
@@ -9,4 +9,37 @@ package leetcode.leetcode2020;
 public class MainOctober {
 
 
+    /**
+     * 已通过题目 139
+     * 提交未通过题目 7
+     * 未开始题目 1667
+     * 提交总数 460
+     * 通过的提交 227
+     * 提交通过率 49.35%
+     */
+
+
+    /**
+     * 70. 爬楼梯
+     *
+     * @param n
+     * @return
+     */
+    public int climbStairs(int n) {
+        /**
+         * f(x) = f(x-1) + f(x-2)
+         */
+        int f1 = 1;
+        int f2 = 2;
+        int sum = 0;
+        for (int i = 3; i <= n; i++) {
+            sum = f1 + f2;
+            f1 = f2;
+            f2 = sum;
+        }
+
+        return sum;
+    }
+
+
 }
