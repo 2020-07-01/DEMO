1cc3b97e47b745145860bef59ae8002fe5ca56d5
2020-07-01 <2422321558@qq.com>
2020-10-03 23:04:59 +0800

    面试题 10.01. 合并排序的数组 面试题 02.02. 返回倒数第 k 个节点

---
+++ JavaExercise/src/main/java/leetcode/leetcode2020/ListNode.java
@@ -0,0 +1,17 @@
+package leetcode.leetcode2020;
+
+/**
+ * @ClassName : ListNode
+ * @Author : yq
+ * @Date: 2020-10-03
+ * @Description : leetCode 节点类
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
--- JavaExercise/src/main/java/leetcode/leetcode2020/MainOctober.java
+++ JavaExercise/src/main/java/leetcode/leetcode2020/MainOctober.java
@@ -52,22 +52,133 @@ public class MainOctober {
 
         int first = 0;
         int second = 0;
-        for (int i = 0; i < cost.length;i++) {
+        for (int i = 0; i < cost.length; i++) {
             int temp = second;
-            second = Math.min(cost[i] + first,second + cost[i]);
+            second = Math.min(cost[i] + first, second + cost[i]);
 
             first = temp;
         }
 
-        return Math.min(first,second);
+        return Math.min(first, second);
+    }
+
+
+    /**
+     * 面试题 02.02. 返回倒数第 k 个节点
+     * 双指针算法
+     * 时间复杂度O(n)
+     *
+     * @param head
+     * @param k
+     * @return
+     */
+    public int kthToLast(ListNode head, int k) {
+
+        if (head == null) {
+            return -1;
+        }
+
+        if (k < 1) {
+            return -1;
+        }
+
+        int right = 1;
+        ListNode node = head;
+        while (right <= k && head != null) {
+            head = head.next;
+            right++;
+        }
+
+        while (head != null) {
+            head = head.next;
+            node = node.next;
+        }
+
+        return node.val;
+    }
+
+
+    /**
+     * 面试题 10.01. 合并排序的数组
+     * 给定两个排序后的数组 A 和 B，其中 A 的末端有足够的缓冲空间容纳 B。 编写一个方法，将 B 合并入 A 并排序。
+     * 双指针法
+     * 时间复杂度O(m+n)
+     * 空间复杂度O(m+n)
+     *
+     * @param A
+     * @param m
+     * @param B
+     * @param n
+     */
+    public void merge(int[] A, int m, int[] B, int n) {
+
+        int[] C = new int[A.length];
+
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        while (a < m || b < n) {
+            if (a < m && b < n) {
+                if (A[a] < B[b]) {
+                    C[c] = A[a];
+                    a++;
+                } else {
+                    C[c] = B[b];
+                    b++;
+                }
+                c++;
+            } else if (a == m) {
+                C[c] = B[b];
+                b++;
+                c++;
+            } else {
+                C[c] = A[a];
+                a++;
+                c++;
+            }
+
+        }
+        System.arraycopy(C, 0, A, 0, m + n);
+    }
+
+
+    /**
+     * 面试题 10.01. 合并排序的数组
+     * 给定两个排序后的数组 A 和 B，其中 A 的末端有足够的缓冲空间容纳 B。 编写一个方法，将 B 合并入 A 并排序。
+     * 从后往前进行遍历
+     * 时间复杂度O(m+n)
+     * 空间复杂度O(m+n)
+     *
+     * @param A
+     * @param m
+     * @param B
+     * @param n
+     */
+    public void merge1(int[] A, int m, int[] B, int n) {
+
+        while (m > 0 && n > 0) {
+            /**
+             * 从后往前遍历元素，将值较大者放在最后
+             */
+            A[m + n - 1] = A[m - 1] > B[n - 1] ? A[--m] : B[--n];
+        }
+        /**
+         * 如果n > 0;则说还有元素未合并
+         */
+        while (n > 0) {
+            A[n] = B[--n];
+        }
     }
 
 
     public static void main(String[] args) {
 
         MainOctober mainOctober = new MainOctober();
-        int[] array = new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
-        int[] array1 = new int[]{10,15,20};
-        System.out.println(mainOctober.minCostClimbingStairs(array1));
+        int[] array = new int[]{1, 2, 3, 0, 0, 0};
+        int[] array1 = new int[]{2, 5, 8};
+
+        mainOctober.merge(array, 3, array1, 3);
+
+        //System.out.println(mainOctober.minCostClimbingStairs(array1));
     }
 }
