8641dd897795d46796f5cefb05607701d6dc459a
2020-07-01 <2422321558@qq.com>
2020-10-06 22:08:00 +0800

    代码结构调整

--- JavaExercise/src/main/java/designMode/factoryMode/method/Car.java
+++
@@ -1,6 +0,0 @@
-package designMode.factoryMode.method;
-
-public interface Car {
-
-    void name();
-}
--- JavaExercise/src/main/java/designMode/factoryMode/method/CarFactory.java
+++
@@ -1,6 +0,0 @@
-package designMode.factoryMode.method;
-
-public interface CarFactory {
-
-    Car getCar();
-}
