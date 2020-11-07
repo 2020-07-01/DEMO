ac4b2304cfe7f76abb4781b34513fad7f2d24325
2020-07-01 <2422321558@qq.com>
2020-11-07 21:00:57 +0800

    最长回文串 词典中最长的单词

--- JavaExercise/src/main/java/leetcode/leetcode2020/MainNovember.java
+++ JavaExercise/src/main/java/leetcode/leetcode2020/MainNovember.java
@@ -386,27 +386,28 @@ public class MainNovember {
      * 575. 分糖果
      * 时间复杂度O(n)
      * 空间复杂度O(n)
+     *
      * @param candies
      * @return
      */
     public int distributeCandies(int[] candies) {
 
-        if(candies == null || candies.length == 0){
+        if (candies == null || candies.length == 0) {
             return 0;
         }
 
-        HashMap<Integer,Integer> hashMap = new HashMap();
+        HashMap<Integer, Integer> hashMap = new HashMap();
         int result = 0;
         for (int i : candies) {
-            if(hashMap.containsKey(i)){
+            if (hashMap.containsKey(i)) {
                 continue;
-            }else {
+            } else {
                 result++;
                 //加完这行代码，性能提升一倍
-                if(result == candies.length/2){
+                if (result == candies.length / 2) {
                     return result;
                 }
-                hashMap.put(i,i);
+                hashMap.put(i, i);
             }
         }
         return result;
@@ -418,21 +419,22 @@ public class MainNovember {
      * 时间复杂度O(n)
      * 空间复杂度O(n)
      * 某个字段串是否为某个回文串得排列之一
+     *
      * @param s
      * @return
      */
     public boolean canPermutePalindrome(String s) {
 
-        if(s == null || s.length() == 0){
+        if (s == null || s.length() == 0) {
             return true;
         }
         char[] chars = s.toCharArray();
 
         Set<Character> set = new HashSet<>();
-        for (int i = 0;i<chars.length;i++){
-            if(set.contains(chars[i])){
+        for (int i = 0; i < chars.length; i++) {
+            if (set.contains(chars[i])) {
                 set.remove(chars[i]);
-            }else {
+            } else {
                 set.add(chars[i]);
             }
         }
@@ -442,32 +444,190 @@ public class MainNovember {
 
 
     /**
-     *
      * @param s
      * @return
      */
     public boolean canPermutePalindrome1(String s) {
 
-        if(s == null || s.length() < 3){
+        if (s == null || s.length() < 3) {
             return true;
         }
         char[] chars = s.toCharArray();
 
-        HashMap<Character,Integer> hashMap = new HashMap<>();
-        for (int i = 0;i<chars.length;i++){
-             hashMap.put(chars[i],hashMap.getOrDefault(chars[i],0)+1);
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < chars.length; i++) {
+            hashMap.put(chars[i], hashMap.getOrDefault(chars[i], 0) + 1);
         }
         int i = 0;
-        for(Map.Entry<Character,Integer> entry : hashMap.entrySet()){
-            if(entry.getValue()%2 != 0){
+        for (Map.Entry<Character, Integer> entry : hashMap.entrySet()) {
+            if (entry.getValue() % 2 != 0) {
                 i++;
             }
-            if(i == 2){
+            if (i == 2) {
                 return false;
             }
         }
 
         return true;
+    }
+
+
+    /**
+     * 720. 词典中最长的单词
+     *
+     * @param words
+     * @return
+     */
+    public String longestWord(String[] words) {
+        if (words == null || words.length == 0) {
+            return null;
+        }
+
+        HashMap<String, Boolean> hashMap = new HashMap<>(words.length);
+
+        for (int i = 0; i < words.length; i++) {
+            hashMap.put(words[i], true);
+        }
+        String result = "";
+        Arrays.sort(words, (v1, v2) -> {
+            return v1.length() - v2.length();
+        });
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            String temp = words[i];
+            Boolean p = true;
+            while (temp.length() > 0) {
+                if (hashMap.containsKey(temp)) {
+                    temp = temp.substring(0, temp.length() - 1);
+                } else {
+                    p = false;
+                    break;
+                }
+            }
+            if (p) {
+                if (result.length() == 0) {
+                    result = words[i];
+                } else {
+                    if (result.length() == words[i].length()) {
+                        int index = 0;
+                        while (index >= 0) {
+                            int r1 = result.charAt(index);
+                            int w1 = words[i].charAt(index);
+                            if (r1 > w1) {
+                                result = words[i];
+                                break;
+                            } else if (r1 == w1) {
+                                index++;
+                                continue;
+                            } else {
+                                break;
+                            }
+                        }
+                    } else {
+                        return result;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 409. 最长回文串
+     * 哈希法
+     * @param s
+     * @return
+     */
+    public int longestPalindrome(String s) {
+
+        /**
+         * 找出所有的偶数
+         * 再加1
+         */
+
+        //aa aa
+        int sum = 0;
+        HashMap<Character, Boolean> hashMap = new HashMap<>(s.length());
+        int index = 0;
+        while (index < s.length()) {
+            if (hashMap.containsKey(s.charAt(index))) {
+                hashMap.remove(s.charAt(index));
+                sum+=2;
+            } else {
+                hashMap.put(s.charAt(index), true);
+            }
+            index++;
+        }
+
+        if (hashMap.isEmpty()) {
+            return sum;
+        } else {
+            return sum + 1;
+        }
+    }
+
+    /**
+     * 最长回文串
+     * set法
+     * @param s
+     * @return
+     */
+    public int longestPalindrome1(String s) {
+
+        /**
+         * 找出所有的偶数
+         * 再加1
+         */
+
+        //aa aa
+        int sum = 0;
+        Set<Character> set = new TreeSet<>();
+        int index = 0;
+        while (index < s.length()) {
+            if (set.contains(s.charAt(index))) {
+                set.remove(s.charAt(index));
+                sum+=2;
+            } else {
+                set.add(s.charAt(index));
+            }
+            index++;
+        }
+
+        if (set.isEmpty()) {
+            return sum;
+        } else {
+            return sum + 1;
+        }
+    }
+
+    /**
+     * 最长回文串
+     * set法
+     * @param s
+     * @return
+     */
+    public int longestPalindrome2(String s) {
+
+        int sum = 0;
+        int index = 0;
+        while (index < s.length() && s.length() >= 1){
+            char c = s.charAt(index);
+            String temp = s.substring(index+1);
+            if(temp.contains(Character.toString(c))){
+                temp = temp.replaceFirst(String.valueOf(c),"");
+                sum = sum + 2;
+                index = 0;
+                s = temp;
+            }else {
+                index++;
+            }
+        }
+
+        if(s.length() > 0){
+            return sum+1;
+        }else {
+            return sum;
+        }
     }
 
     public static void main(String[] args) {
@@ -476,10 +636,10 @@ public class MainNovember {
         int[] array = new int[]{1, 2, 2, 1, 1, 3};
 
         //mainNovember.duplicateZeros(array);
-        String[] strings = new String[]{"abdfs", "cccd", "a", "qwwewm"};
+        String[] strings = new String[]{"m", "mo", "moc", "moch", "mocha", "l", "la", "lat", "latt", "latte", "c", "ca", "cat"};
         //System.out.println(Arrays.toString(mainNovember.uncommonFromSentences("this apple is sweet", "this apple is sour")));
 
-        System.out.println(mainNovember.uniqueOccurrences(array));
+        System.out.println(mainNovember.longestPalindrome2("abccccdd"));
     }
 
 }
